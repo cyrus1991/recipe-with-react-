0ae2f50e5579dfae7293f5c84ab1e46f7bbb0389
CYRUS <Cyrus.mohammadi@lokalleads.de>
2018-07-19 11:44:27 +0200

    the searchbar and a bit design are ready

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Form from "./components/Form"
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Recipe Search</h1>
+          <h1 className="App-title">Recipe Search</h1> 
         </header>
+          
+        <Form />
+
+       
       </div>
     );
   }
