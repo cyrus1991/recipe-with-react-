97acb310965fee00bebabbdf3a1280561a2a6220
CYRUS <Cyrus.mohammadi@lokalleads.de>
2018-07-24 11:15:28 +0200

    now is nicer and substring works

--- src/App.js
+++ src/App.js
@@ -2,36 +2,46 @@ import React, { Component } from "react";
 import "./App.css";
 
 import Form from "./components/Form";
+import Recipes from "./components/Recipes"
 const API_KEY = "8736c5284972cf436c829548a186d37a";
 
 class App extends Component {
- 
+  // in react 16 and older we dont neet to use [ this.state ] in the [ constructor] we can simply jsut use state
+  state = {
+    recipes: []
+  };
   // 1.because we use arrow func then we dont need to bind the func!
-  getRecipe =async  (e) => {
-        const recipeName = e.target.elements.recipeName.value
-        e.preventDefault()
-        const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=5`);
-       // 2.to grapb the value of written value of our fetch func we should use keyword [await]  again 
-       //  3.and also we have to convert the result that we get from the fetch to json
-       // 4.so the new method to convert it is like here in the blow
-        const data = await api_call.json()
-        // 5. whatever comes back from the  API will store in the data variable and also convert to json
-        console.log(data)
-        /* 6. so because I'm useing the  local server the website that I wanna fetch the data from
+  getRecipe = async e => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(
+      `https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`
+    );
+    // 2.to grapb the value of written value of our fetch func we should use keyword [await]  again
+    //  3.and also we have to convert the result that we get from the fetch to json
+    // 4.so the new method to convert it is like here in the blow
+    const data = await api_call.json();
+    // 5. whatever comes back from the  API will store in the data variable and also convert to json
+
+    /* 6. so because I'm useing the  local server the website that I wanna fetch the data from
         doesnt let me to get access to it so to fix the problem there is a trick 
         we could add this url at the very begining of our url https://cors-anywhere.herokuapp.com/
         so it tricks the website that we are a live server 
         */
-      
-      
-  }
+    this.setState({
+      recipes: data.recipes
+    });
+    console.log(this.state.recipes);
+  };
   render() {
     return (
       <div className="App">
-          <header className="App-header">
-            <h1 className="App-title">Recipe Search</h1>
-          </header>
-          <Form getRecipe= {this.getRecipe} />
+        <header className="App-header">
+          <h1 className="App-title">Recipe Search</h1>
+        </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes ={this.state.recipes}></Recipes>
+        
       </div>
     );
   }
---
+++ src/components/Recipes.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Recipes = props => (
+  <div className="container">
+    <div className="row">
+            {props.recipes.map(recipe => {
+                return (
+                /* because I used map react wants to know each item which I get back from 
+                        the map method specifically so for that we have to give each item  a 
+                        soecefic key so I used Math.random() which I could also use thier names or 
+                        anythin which is unique! */
+        
+                <div key = {Math.random() }className="col-md-4" style ={{marginBottom: "2rem"}} >
+                        <div className="recipes__box">
+                                <img
+                                        className="recipe__box-img"
+                                        src ={recipe.image_url}
+                                        alt ={recipe.title} />
+                                 <div className="recipe__text">
+                                        <h4 className="recipes__title">
+                                                {recipe.title.length <  20  ? `${recipe.title}` : `${recipe.title.substring(0, 25)}...` }
+                                        </h4>
+                                        <p className="recipes__subtitle">Publisher: <span>
+                                        {recipe.publisher}
+                                        </span></p>
+                                 </div>
+                                 <button className="recipe_buttons">View Recipe</button>
+                        </div>
+                </div>
+                );
+            })}
+  
+     </div>
+    
+  </div>
+);
+
+export default Recipes;
+
